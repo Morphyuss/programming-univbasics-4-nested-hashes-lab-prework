52ee49f935c4c62436282118b9eb01e829f7f75e
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 17:08:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].class
+		 programmer_hash[:dennis_ritchie][:languages].value
 end
 
 def adding_matz
